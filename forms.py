731182f4a97acbfe5379975a218ebe5bbcd8eb22
aaprile2 <allisonnicoleaprile@gmail.com>
2020-03-25 21:17:25 -0400

    removed flask_uploads

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, RadioField
-from flask_uploads import UploadSet, IMAGES
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class filter(FlaskForm):
